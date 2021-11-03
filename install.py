775f7cedb56f014279daa34d1b240cac4979321f
Mayukh Kundu <mzk0148@auburn.edu>
2021-11-03 11:17:41 -0500

    Fix installation target option.

--- install.py
+++ install.py
@@ -26,7 +26,7 @@ import shutil
 import sys
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('dest', type=str, nargs='?', default=None, help='Destination')
+parser.add_argument('-o', dest='dest', type=str, default=None, help='Destination')
 parser.add_argument('-j', dest='journal', type=str, choices=['all','acs','aip'], nargs='+', default=['all'], help='Journal styles')
 args = parser.parse_args()
 
